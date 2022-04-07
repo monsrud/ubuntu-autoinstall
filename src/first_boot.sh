877e7cde372932b4b6de6597d431d9d785e223e5
Marshall Onsrud <marshall.onsrud@gmail.com>
2022-04-07 14:11:57 -0500

    update

--- src/first_boot.sh
+++ src/first_boot.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 # generate ssh key
-ssh-keygen -q -t edcsa -N '' -f /root/.ssh/id_edcsa
-cp /root/.ssh/id_edcsa.pub  /root/.ssh/authorized_keys 
+mkdir -p /root/.ssh
+chmod go-rwx /root/.ssh
+ssh-keygen -q -t ecdsa -N '' -f /root/.ssh/id_edcsa
+cp /root/.ssh/id_ecdsa.pub  /root/.ssh/authorized_keys 
 
 git clone https://github.com/monsrud/ubuntu-autoinstall.git
 cd ubuntu-autoinstall/ansible
