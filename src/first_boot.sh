f4f4ce151ba98ff577ddcf174d0403fa2a94f9d8
Marshall Onsrud <marshall.onsrud@gmail.com>
2022-04-07 14:22:33 -0500

    oops

--- src/first_boot.sh
+++ src/first_boot.sh
@@ -3,7 +3,7 @@
 # generate ssh key
 mkdir -p /root/.ssh
 chmod go-rwx /root/.ssh
-ssh-keygen -q -t ecdsa -N '' -f /root/.ssh/id_edcsa
+ssh-keygen -q -t ecdsa -N '' -f /root/.ssh/id_ecdsa
 cp /root/.ssh/id_ecdsa.pub  /root/.ssh/authorized_keys 
 
 git clone https://github.com/monsrud/ubuntu-autoinstall.git
